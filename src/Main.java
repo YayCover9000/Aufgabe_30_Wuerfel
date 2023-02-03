8422a78daace9f55f14e0a4709d82a9a311d9444
YayCover9000 <yassuanf@googlemail.com>
2023-02-03 16:13:05 +0100

    added pasch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
         int raus = zufallszahlGenerator.generiereZufallszahl(1);
-        System.out.println(raus);
+        //System.out.println(raus);
         int wuerfel[] = new int[2];
         wuerfel[1] = (raus % 6) + 1;
         while (true){
