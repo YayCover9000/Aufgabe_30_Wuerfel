eca52ed44a6ea1893b8502d7bc1c97c39a91c02b
YayCover9000 <yassuanf@googlemail.com>
2023-02-03 16:10:51 +0100

    added array for wiuerfel wuerfe.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,20 @@ public class Main {
         System.out.println("Hello world!");
         int raus = zufallszahlGenerator.generiereZufallszahl(1);
         System.out.println(raus);
+        int wuerfel[] = new int[2];
+        wuerfel[1] = (raus % 6) + 1;
+        while (true){
+            System.out.println("Würfel 1: " + wuerfel[0]);
+            System.out.println("Würfel 2: " + wuerfel[1]);
+            if (wuerfel[0] == wuerfel[1]){
+                System.out.println("Pasch!");
+                break;
+            }
+            wuerfel[0] = wuerfel[1];
+            raus = zufallszahlGenerator.generiereZufallszahl(1);
+            wuerfel[1] = (raus % 6) + 1;
+        }
+        System.out.println(wuerfel[1]);
     }
 
 }
--- src/zufallszahlGenerator.java
+++ src/zufallszahlGenerator.java
@@ -2,12 +2,10 @@ import java.lang.Math;;
 public class zufallszahlGenerator {
     private static int shortPlaceHolder = 0;
     public static int generiereZufallszahl(long samen) {
-        long xK = systemZeit.getSystemZeit();
+        long xK = systemZeit.getSystemZeit(); //System.currentTimeMillis();
         int a = 16807;
         double m = Math.pow(2, 31) - 1;
-        //samen = (a * samen) % m;
         double xK1 = (a * xK) % m;
-        //long ausgabe = (int) (samen % 100);
         return (int) xK1;
     }
 
