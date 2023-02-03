8da61aa39835ac80abbcf5fb0b28cca871809f1b
YayCover9000 <yassuanf@googlemail.com>
2023-02-03 16:25:07 +0100

    added 10 rolls and timer timeout

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //System.out.println("Hello world!");
-        int raus = zufallszahlGenerator.generiereZufallszahl(1);
-        //System.out.println(raus);
-        int wuerfel[] = new int[2];
-        wuerfel[1] = (raus % 6) + 1;
-        while (true){
-            System.out.println("Würfel 1: " + wuerfel[0]);
-            System.out.println("Würfel 2: " + wuerfel[1]);
-            if (wuerfel[0] == wuerfel[1]){
-                System.out.println("Pasch!");
-                break;
-            }
-            wuerfel[0] = wuerfel[1];
-            raus = zufallszahlGenerator.generiereZufallszahl(1);
-            wuerfel[1] = (raus % 6) + 1;
+        for(int i = 0; i < 10; i++) {
+            int raus = zufallszahlGenerator.generiereZufallszahl(System.currentTimeMillis());
+            //System.out.println(raus);
+            int wuerfel[] = new int[2];
+            wuerfel[0] = (raus % 6) + 1;
+            System.out.println(wuerfel[0]);
+            java.util.concurrent.TimeUnit.SECONDS.sleep(1);
         }
-        System.out.println(wuerfel[1]);
+
     }
 
 }
