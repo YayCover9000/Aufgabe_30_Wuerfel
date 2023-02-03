13ec7aed1cd66f816aa9e9cd9509e3a4734ffb6d
YayCover9000 <yassuanf@googlemail.com>
2023-02-03 15:17:39 +0100

    added math.pow()

--- src/zufallszahlGenerator.java
+++ src/zufallszahlGenerator.java
@@ -1,11 +1,12 @@
+import java.lang.Math;
 public class zufallszahlGenerator {
     private static int shortPlaceHolder = 0;
     public static int generiereZufallszahl(long samen) {
         int xK = 213;
         int a = 16807;
-        int m = 2^3;
+        double m = Math.pow(2, 31) - 1;
         //samen = (a * samen) % m;
-        int xK1 = (a * xK) % m;
+        double xK1 = (a * xK) % m;
         long ausgabe = (int) (samen % 100);
         return (int) ausgabe;
     }
