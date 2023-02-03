061ae9a7ffc03b460094b20dd1990b265cde8b12
YayCover9000 <yassuanf@googlemail.com>
2023-02-03 15:27:52 +0100

    outcomment ausgabe

--- src/zufallszahlGenerator.java
+++ src/zufallszahlGenerator.java
@@ -2,13 +2,13 @@ import java.lang.Math;;
 public class zufallszahlGenerator {
     private static int shortPlaceHolder = 0;
     public static int generiereZufallszahl(long samen) {
-        int xK = 213;
+        long xK = systemZeit.getSystemZeit();
         int a = 16807;
         double m = Math.pow(2, 31) - 1;
         //samen = (a * samen) % m;
         double xK1 = (a * xK) % m;
-        long ausgabe = (int) (samen % 100);
-        return (int) ausgabe;
+        //long ausgabe = (int) (samen % 100);
+        return (int) xK1;
     }
 
 }
